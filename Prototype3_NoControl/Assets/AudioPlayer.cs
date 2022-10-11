2d3feca3f144d675ea67c45ddc2437b356e51822
masterdd15 <masterdd15@gmail.com>
2022-10-10 20:53:35 -0400

    AddedInTimer
    Creating a timer to see the ridiculousness of waiting on managment

--- Prototype3_NoControl/Assets/AudioPlayer.cs
+++ Prototype3_NoControl/Assets/AudioPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AudioPlayer : MonoBehaviour
 {
@@ -10,18 +11,32 @@ public class AudioPlayer : MonoBehaviour
     public AudioClip introClip;
     public AudioClip startTimer;
 
+    //This is were our timer is set
+    public float timer;
+    public TextMeshProUGUI timerText;
+    public GameObject timerObject;
+
+    //Bools to track our states
+    private bool isCounting;
+
     
     // Start is called before the first frame update
     void Start()
     {
         myAudio = GetComponent<AudioSource>();
         StartCoroutine(PlayIntro());
+        timer = 50f;
+        isCounting = false;
+        timerObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isCounting)
+        {
+            StartCountdown();
+        }
     }
 
     IEnumerator PlayIntro()
@@ -31,5 +46,14 @@ public class AudioPlayer : MonoBehaviour
         yield return new WaitForSeconds(myAudio.clip.length);
         myAudio.clip = startTimer;
         myAudio.Play();
+        yield return new WaitForSeconds(myAudio.clip.length);
+        isCounting = true;
+    }
+
+    public void StartCountdown()
+    {
+        timerObject.SetActive(true);
+        timer -= Time.deltaTime;
+        timerText.text = timer.ToString();
     }
 }
